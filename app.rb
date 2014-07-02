a702faabccd6ac5e6529a965def65a07b0a21b47
Hans Dushanthakumar <hans@thoughtworks.com>
2014-07-03 09:30:39 +1000

    Updates to the quote form - its now in a box

--- app.rb
+++ app.rb
@@ -1,12 +1,22 @@
 require 'sinatra'
 require 'haml'
+require 'v8'
+require 'coffee-script'
 
 get '/' do
   haml :index
 end
 
+get '/test' do
+  erb :test
+end
+
 post '/quote' do
   haml :quote
+end
+
+get '/javascripts/application.js' do
+  coffee :application
 end
 
 not_found do
