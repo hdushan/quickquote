d280e09c929f873235fb6501d9c5e60f4fe88ab3
Hans Dushanthakumar <hans@thoughtworks.com>
2014-07-09 18:25:19 +1000

    Removed external dependancies for jquery css/js

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,26 +1,18 @@
 require 'capybara/cucumber'
 require 'phantomjs'
 require_relative '../../app'
+require 'selenium-webdriver'
 
-if ENV['CH'] == 'true' # in case you wanna run it with selenium
-  require 'selenium-webdriver'
+if ENV['CH'] == 'true'
   Capybara.register_driver :selenium do |app|
     Capybara::Selenium::Driver.new(app, :browser => :chrome)
   end
 else
-  require 'capybara/poltergeist'
-  Capybara.register_driver :poltergeist do |app|
-    Capybara::Poltergeist::Driver.new(app, {
-      js_errors: true,
-      inspector: true,
-      phantomjs_options: ['--load-images=no', '--ignore-ssl-errors=yes'],
-      timeout: 120
-    })
+  Capybara.register_driver :selenium do |app|
+    Capybara::Selenium::Driver.new(app, :browser => :phantomjs)
   end
-  Capybara.javascript_driver = :poltergeist
 end
 
-Capybara.current_driver = ENV['CH'] == 'true' ? :selenium : :poltergeist
 Capybara.app = App.new
 
 
