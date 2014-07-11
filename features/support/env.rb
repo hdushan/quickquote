e0075e196f5e462acf61c98c3a2b8748dc00a784
Hans Dushanthakumar <hans@thoughtworks.com>
2014-07-11 16:10:09 +1000

    Intial go at selenium grid

--- features/support/env.rb
+++ features/support/env.rb
@@ -3,17 +3,25 @@ require 'phantomjs'
 require_relative '../../app'
 require 'selenium-webdriver'
 
-class Capybara::Server
-private
-  def find_available_port
-    @port = 9887 + ENV['TEST_ENV_NUMBER'].to_i
-    Capybara.server_port = @port
-  end
-end
+#class Capybara::Server
+#private
+#  def find_available_port
+#    @port = 9887 + ENV['TEST_ENV_NUMBER'].to_i
+#    Capybara.server_port = @port
+#  end
+#end
 
 if ENV['CH'] == 'true'
   Capybara.register_driver :selenium do |app|
     Capybara::Selenium::Driver.new(app, :browser => :chrome)
+  end
+elsif ENV['REMOTE'] == 'true'
+  caps = Selenium::WebDriver::Remote::Capabilities.chrome
+  Capybara.register_driver :selenium do |app|
+    Capybara::Selenium::Driver.new(app,
+      :browser => :remote,
+      :url => "http://localhost:4444/wd/hub",
+      :desired_capabilities => caps)
   end
 else
   Capybara.register_driver :selenium do |app|
